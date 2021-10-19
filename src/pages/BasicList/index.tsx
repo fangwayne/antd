f1d4107f2e1337f055c4bd7f96ccabdafcd2eada
fangwayne <13555576@qq.com>
2021-10-19 16:22:03 +0800

    push origin

--- src/pages/BasicList/index.tsx
+++ src/pages/BasicList/index.tsx
@@ -2,6 +2,7 @@
 // 第二次更新
 // 第三次更新（服务端更新; 客户端用 git pull origin 命令拉取, 成功!）
 // 第四次修改（客户端用 git pull 命令拉取)
+// 5: push origin
 import { useState, useEffect } from 'react';
 import { Table, Space, Row, Col, Card, Pagination } from 'antd';
 import { useRequest } from 'umi';
