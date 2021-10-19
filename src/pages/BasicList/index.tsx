0320e14b2bace0b74d08a1a53e98b1332f539601
fangwayne <13555576@qq.com>
2021-10-19 17:04:17 +0800

    commit commit.txt file to local repository

--- src/pages/BasicList/index.tsx
+++ src/pages/BasicList/index.tsx
@@ -3,6 +3,7 @@
 // 第三次更新（服务端更新; 客户端用 git pull origin 命令拉取, 成功!）
 // 第四次修改（客户端用 git pull 命令拉取)
 // 5: push origin
+// 6: the diff 
 import { useState, useEffect } from 'react';
 import { Table, Space, Row, Col, Card, Pagination } from 'antd';
 import { useRequest } from 'umi';
