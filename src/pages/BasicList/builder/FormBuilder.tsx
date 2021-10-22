996572b6fe5bc6456819598d9fb7aa8efb6b8173
fangwayne <13555576@qq.com>
2021-10-22 17:28:12 +0800

    2021年10月22日17:27:33
    FromBuilder established

--- src/pages/BasicList/builder/FormBuilder.tsx
+++ src/pages/BasicList/builder/FormBuilder.tsx
@@ -1,14 +1,41 @@
 //import React from 'react';
-
-import { Form, Input } from 'antd';
+import { Form, Input, DatePicker, Switch, TreeSelect } from 'antd';
+//import TreeSelect from 'rc-tree-select';
 
 const FormBuilder = (data: PageApi.Datum[] | undefined) => {
   return (data || []).map((field) => {
-    return (
-      <Form.Item name={field.key} label={field.title}>
-        <Input />
-      </Form.Item>
-    );
+    switch (field.type) {
+      case 'text':
+        return (
+          <Form.Item name={field.key} label={field.title} key={field.key}>
+            <Input disabled={field.disabled} />
+          </Form.Item>
+        );
+      case 'datetime':
+        return (
+          <Form.Item name={field.key} label={field.title} key={field.key}>
+            <DatePicker showTime disabled={field.disabled} />
+          </Form.Item>
+        );
+      case 'tree':
+        return (
+          <Form.Item name={field.key} label={field.title} key={field.key}>
+            <TreeSelect treeData={field.data} disabled={field.disabled} />
+          </Form.Item>
+        );
+      case 'switch':
+        return (
+          <Form.Item name={field.key} label={field.title} key={field.key}>
+            <Switch
+              //checkedChildren="开启" unCheckedChildren="关闭" defaultChecked
+              //onChange={onChange}
+              disabled={field.disabled}
+            />
+          </Form.Item>
+        );
+      default:
+        return null;
+    }
   });
 };
 
--- src/pages/BasicList/component/Modal.tsx
+++ src/pages/BasicList/component/Modal.tsx
@@ -3,7 +3,8 @@ import { Modal as AntdModal, Form } from 'antd';
 import { useRequest } from 'umi';
 import FormBuilder from '../builder/FormBuilder';
 import ActionBuilder from '../builder/ActionBuilder';
-// 2021年10月21日21:52:19 在家测试上传
+import moment from 'moment';
+
 const Modal = ({
   modalVisible,
   handleModal,
@@ -13,6 +14,7 @@ const Modal = ({
   handleModal: () => void;
   modalUri: string;
 }) => {
+  const [form] = Form.useForm();
   const init = useRequest<{ data: PageApi.Data }>(`${modalUri}`);
 
   useEffect(() => {
@@ -20,6 +22,46 @@ const Modal = ({
       init.run();
     }
   }, [modalVisible]);
+
+  const adaptor = (data: PageApi.Data) => {
+    if (data?.layout?.tabs && data?.dataSource) {
+      const result = {};
+      data?.layout?.tabs.forEach((tab) => {
+        tab.data.forEach((field) => {
+          switch (field.type) {
+            case 'datetime':
+              result[field.key] = moment(data.dataSource[field.key]);
+              break;
+            default:
+              result[field.key] = data.dataSource[field.key];
+              break;
+          }
+        });
+      });
+      return result;
+    }
+  };
+
+  useEffect(() => {
+    if (init.data) {
+      //form.setFieldsValue(init.data?.dataSource);
+      form.setFieldsValue(
+        //{
+        // ...init.data.dataSource,
+        // create_time: moment("2020-10-22T15:38:51+08:00"),
+        // or
+        // "username": "admin0",
+        // "id": 206,
+        // "display_name": "",
+        // "create_time": moment("2020-10-22T15:38:51+08:00"),
+        // "update_time": moment("2020-10-31T13:28:21+08:00"),
+        // "status": 1,
+        // "groups": [53]
+        // }
+        adaptor(init?.data),
+      );
+    }
+  }, [init.data]);
 
   const layout = {
     labelCol: { span: 8 },
@@ -35,7 +77,9 @@ const Modal = ({
         onCancel={handleModal}
         footer={ActionBuilder(init.data?.layout.actions[0].data)}
       >
-        <Form {...layout}>{FormBuilder(init.data?.layout?.tabs[0]?.data)}</Form>
+        <Form {...layout} form={form}>
+          {FormBuilder(init.data?.layout?.tabs[0]?.data)}
+        </Form>
       </AntdModal>
     </div>
   );
